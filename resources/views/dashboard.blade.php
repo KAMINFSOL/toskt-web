42b96a9c786141b02ace2d913aa323525b9d8e3e
KAMINFSOL <kaminfsol@yandex.ru>
2026-09-04 22:10:23 +0300

    feat: add new dashboard and prepare files connect

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    <h1 class="text-blue-400">Hello world!</h1>
+{{-- 
+    Проект: toskt-web
+    Файл: dashboard.blade.php
+    Автор: Copyright (c) 2026, Фесянов Илья (Fesyanov Ilya)
+--}}
+
+@include('layouts.header', ['title' => 'Главная оТОиСКТ', 'headerTitle' => 'Отдел ТОиСКТ'])
+    <main>
+        <div class="flex flex-col justify-center items-center mt-10">
+            <h2 class="text-2xl font-bold mb-4">С возвращением, {{ auth()->user()->name }}!</h2>
+            <div class="flex gap-3 mt-4 sm:mt-1 p-6 sm:p-5 mx-auto rounded-xl max-w-370 w-full">
+                @foreach ($requests as $request)
+                    <div class="flex items-center gap-5 bg-white/80 backdrop-blur-xl p-3 sm:p-5 my-2 mx-auto rounded-xl shadow-xl w-full">
+                        <div class="">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-15 text-blue-400">
+                                <path d="M19.5 22.5a3 3 0 0 0 3-3v-8.174l-6.879 4.022 3.485 1.876a.75.75 0 1 1-.712 1.321l-5.683-3.06a1.5 1.5 0 0 0-1.422 0l-5.683 3.06a.75.75 0 0 1-.712-1.32l3.485-1.877L1.5 11.326V19.5a3 3 0 0 0 3 3h15Z" />
+                                <path d="M1.5 9.589v-.745a3 3 0 0 1 1.578-2.642l7.5-4.038a3 3 0 0 1 2.844 0l7.5 4.038A3 3 0 0 1 22.5 8.844v.745l-8.426 4.926-.652-.351a3 3 0 0 0-2.844 0l-.652.351L1.5 9.589Z" />
+                            </svg>
+                        </div>
+                        <div class="">
+                            <h3 class="font-bold flex items-center gap-3">
+                                {{ $request->number }} 
+                                {{ Carbon\Carbon::parse($request->created_at)->format('d.m.Y') }}
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-5 text-blue-400">
+                                    <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                                </svg>
+                            </h3>
+                            <div class="">
+                                <span>{{ $request->from }} {{ $request->subdivision }} {{ $request->cabinet }}</span>
+                            </div>
+                            <span>{{ $request->description }}</span>
+                            <div class="">
+                                <span>{{ $request->telephone }}</span>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="flex flex-col justify-center items-center mt-10">
+            <h2 class="text-2xl font-bold mb-4">Основная информация</h2>
+            <div class="flex flex-col items-center justify-center gap-3 mt-4 sm:mt-1 p-6 sm:p-5 mx-auto rounded-xl max-w-370 w-full">
+                @include('partials.files')
+            </div>
+        </div>
+    </main>
+    @include('layouts.footer_full')
 </body>
 </html>
---
+++ resources/views/partials/files.blade.php
@@ -0,0 +1,18 @@
+@foreach ($files as $file)
+    <div class="flex justify-between bg-blue-400 w-300 p-5 rounded-xl">
+        <div class="flex gap-3">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-white">
+                <path fill-rule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625ZM7.5 15a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 7.5 15Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H8.25Z" clip-rule="evenodd" />
+                <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
+            </svg>
+            <h4 class="text-white font-bold">{{ $file->name }}</h4>
+        </div>
+        <div class="">
+            <a href="{{ route('download.file', $file->id) }}">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-white">
+                    <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75Zm-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+                </svg>
+            </a>
+        </div>
+    </div>
+@endforeach
